188f779dad1cb2802823b1d625fb554f3a661224
Joao Pedro <joaopedro.santosoliveira2003@gmail.com>
2022-09-22 12:16:12 -0300

    atividade props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Ola(props){
+  return <h1>Olá, {props.name}</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div>
+      <Ola name="João"/>
+     </div>
   );
 }
 
